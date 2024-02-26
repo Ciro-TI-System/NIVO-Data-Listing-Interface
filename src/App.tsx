2a33f72e58e29e20e2195e6f3f194bef3df52a12
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2024-02-26 16:24:20 -0300

    Import of Filter and Export components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Plus } from "lucide-react";
+import { FileDown, Plus, Search } from "lucide-react";
 import { Header } from "./components/header";
 import { Tabs } from "./components/tabs";
 import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
 
 export function App() {
   return (
@@ -14,9 +15,21 @@ export function App() {
         <main className="max-w-6xl mx-auto space-y-5">
           <div className="flex items-center gap-3">
             <h1 className="text-xl font-bold">Tags</h1>
-            <Button variant='primary'>
+            <Button variant="primary">
               <Plus className="size-3" />
               Create new
+            </Button>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <Input variant="filter">
+              <Search className="size-3" />
+              <Control placeholder="Search tags..." />
+            </Input>
+
+            <Button>
+              <FileDown className="size-3" />
+              Export
             </Button>
           </div>
         </main>
